0debfa03ff2d3f345f5700a092bdd341a9377dbf
pari169 <patelpari162009@gmail.com>
2026-09-14 00:40:37 +0530

    Implement frequency counting in program2.c

---
+++ program2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+#define MAXVAL 50
+#define COUNTER 11
+
+itn main()
+{
+      float value[MAXVAL];
+      int i, low, high;
+      int group[COUNTER] = {0,0,0,0,0,0,0,0,0,0,0};
+
+    /*. . . . .READING AND COUNTING. . . . .*/
+
+for(i = 0; i< MAXVAL; i++)
+{
+      scanf("%f", &value[i]);
+      group[(int)(value[i] / 10)]++;
+}
+
+/*. . . . .PRINTING OF FREQUENCY TABLE. . . . .*/
+
+printf("\n");
+printf("GROUP RANGE FREQUENCY\N");
+
+for(i = 0; i< COUNTER; i++)
+{
+    low = i*10;
+
+      if(i == 10)
+        high = 100;
+      else
+        high = low + 9;
+
+      printf("%2d %3d to %3d %5d\n", i + 1, low, high, group[i]);
+}
+
+return 0;
+
+}
